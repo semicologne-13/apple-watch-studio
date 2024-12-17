022a55279ea113cb253fb9343aa11738b86b0648
Alen Cyriac Joseph <colognesemi@gmail.com>
2024-12-17 10:33:56 +0530

    Set up Next.js 15 with Tailwind CSS
    Installed Next.js 15.
    Removed default Next.js boilerplate (e.g., sample pages, styles).
    Prepared project structure for custom development.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="text-6xl font-extrabold p-40 text-center">
+      <p className="text-3xl bg-center font-thin">Apple Watch Studio</p>
+      Choose a case.
+      <br/>
+      Pick a brand.
+      <br/>
+      Create your own style.
+    </div>
+  );
+}
